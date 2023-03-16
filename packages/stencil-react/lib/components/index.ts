5b3adff9e8f85f88ccdef291f9b09a50dceefdd1
Carlos Saito <carlos@saito.style>
2023-03-16 18:08:21 +0100

    Fix module resolution

--- packages/stencil-react/lib/components/index.ts
+++ packages/stencil-react/lib/components/index.ts
@@ -2,11 +2,10 @@
 /* tslint:disable */
 /* auto-generated react proxies */
 import { createReactComponent } from "./react-component-lib";
+import type { JSX } from "stencil-wc/components";
 
-import type { JSX } from "stencil-wc/dist";
-
-import { defineCustomElement as defineKthButton } from "stencil-wc/dist/components/kth-button.js";
-import { defineCustomElement as defineMyComponent } from "stencil-wc/dist/components/my-component.js";
+import { defineCustomElement as defineKthButton } from "stencil-wc/components/kth-button.js";
+import { defineCustomElement as defineMyComponent } from "stencil-wc/components/my-component.js";
 
 export const KthButton = /*@__PURE__*/ createReactComponent<
   JSX.KthButton,
