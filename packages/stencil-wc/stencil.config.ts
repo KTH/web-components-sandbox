d9089b0091445a3c303f0cde1587af975ae25376
Carlos Saito <carlos@saito.style>
2023-03-16 17:37:40 +0100

    Document changes

--- packages/stencil-wc/stencil.config.ts
+++ packages/stencil-wc/stencil.config.ts
@@ -4,19 +4,24 @@ import { reactOutputTarget } from '@stencil/react-output-target';
 export const config: Config = {
   namespace: 'stencil-wc',
   outputTargets: [
+    // This is required if we intend to ship Web Components in a CDN
+    // {
+    //   type: 'dist',
+    //   esmLoaderPath: '../loader',
+    // },
     {
-      type: 'dist',
-      esmLoaderPath: '../loader',
+      // This generates all Web Components separately, so they can be imported
+      // without any extras.
+
+      // Works better for importing in React Apps
+      type: 'dist-custom-elements',
+      generateTypeDeclarations: true,
     },
     reactOutputTarget({
       componentCorePackage: 'stencil-wc',
       proxiesFile: '../stencil-react/lib/components/index.ts',
       includeImportCustomElements: true,
     }),
-    {
-      type: 'dist-custom-elements',
-      generateTypeDeclarations: true,
-    },
     {
       type: 'www',
       serviceWorker: null, // disable service workers
